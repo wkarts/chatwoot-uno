4e5e3aa5e39af1182b0ba7499b4db8c57cb4c173
Clairton Rodrigo Heinzen <clairton.rodrigo@gmail.com>
2024-02-12 16:04:18 -0300

    add feature agent viewed

--- app/jobs/update_last_seen_job.rb
+++ app/jobs/update_last_seen_job.rb
@@ -2,26 +2,34 @@ class UpdateLastSeenJob < ApplicationJob
   queue_as :default
 
   def perform(conversation_id, user, agent_last_seen_at)
+    agent_viewed(conversation_id, user) if conversation.account.feature_enabled?('agent_conversation_viewed')
+
+    read_message(conversation_id, agent_last_seen_at) if conversation.account.feature_enabled?('read_message')
+  end
+
+  private
+
+  def read_message(conversation_id, agent_last_seen_at)
     conversation = Conversation.find(conversation_id)
-
-    key = "conversation:last_seen:#{conversation_id}:#{user.id}"
-    unless ::Redis::Alfred.get(key)
-      ::Redis::Alfred.setex(key, true, 1.hour)
-      params = {
-        message_type: :activity,
-        content_type: :text,
-        content: "Conversa visualizada por #{user.id} - #{user.name}"
-      }
-      Messages::MessageBuilder.new(user, conversation, params).perform
-    end
-
-    return unless conversation.account.feature_enabled?('read_message')
-
     messages = conversation.messages.to_read(agent_last_seen_at)
     Rails.logger.debug { "Conversation #{conversation_id} with #{messages.size} message(s) to update status to read" }
     messages.each do |message|
       Rails.logger.debug { "Update message id #{message.id} source #{message.source_id} status to read" }
       message.update(status: :read)
     end
+  end
+
+  def agent_viewed(conversation_id, user)
+    conversation = Conversation.find(conversation_id)
+    key = "conversation:last_seen:#{conversation_id}:#{user.id}"
+    return if ::Redis::Alfred.get(key)
+
+    ::Redis::Alfred.setex(key, true, 1.hour)
+    params = {
+      message_type: :activity,
+      content_type: :text,
+      content: "Conversa visualizada por #{user.id} - #{user.name}"
+    }
+    Messages::MessageBuilder.new(user, conversation, params).perform
   end
 end
