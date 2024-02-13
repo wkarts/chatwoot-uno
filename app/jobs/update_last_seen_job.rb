cf1ed810d3d45a49be5ebc0788ee0bf38cffa8f0
Clairton Rodrigo Heinzen <clairton.rodrigo@gmail.com>
2024-02-13 08:48:07 -0300

    fix update last seen

--- app/jobs/update_last_seen_job.rb
+++ app/jobs/update_last_seen_job.rb
@@ -2,26 +2,26 @@ class UpdateLastSeenJob < ApplicationJob
   queue_as :default
 
   def perform(conversation_id, user, agent_last_seen_at)
-    agent_viewed(conversation_id, user) if conversation.account.feature_enabled?('agent_conversation_viewed')
+    conversation = Conversation.find(conversation_id)
 
-    read_message(conversation_id, agent_last_seen_at) if conversation.account.feature_enabled?('read_message')
+    agent_viewed(conversation, user) if conversation.account.feature_enabled?('agent_conversation_viewed')
+
+    read_message(conversation, agent_last_seen_at) if conversation.account.feature_enabled?('read_message')
   end
 
   private
 
-  def read_message(conversation_id, agent_last_seen_at)
-    conversation = Conversation.find(conversation_id)
+  def read_message(conversation, agent_last_seen_at)
     messages = conversation.messages.to_read(agent_last_seen_at)
-    Rails.logger.debug { "Conversation #{conversation_id} with #{messages.size} message(s) to update status to read" }
+    Rails.logger.debug { "Conversation #{conversation.id} with #{messages.size} message(s) to update status to read" }
     messages.each do |message|
       Rails.logger.debug { "Update message id #{message.id} source #{message.source_id} status to read" }
       message.update(status: :read)
     end
   end
 
-  def agent_viewed(conversation_id, user)
-    conversation = Conversation.find(conversation_id)
-    key = "conversation:last_seen:#{conversation_id}:#{user.id}"
+  def agent_viewed(conversation, user)
+    key = "conversation:last_seen:#{conversation.id}:#{user.id}"
     return if ::Redis::Alfred.get(key)
 
     ::Redis::Alfred.setex(key, true, 1.hour)
