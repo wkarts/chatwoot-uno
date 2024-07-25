5d085f38ef484a9565359ce51f5caa703bbe02c0
Clairton Rodrigo Heinzen <clairton.rodrigo@gmail.com>
2024-07-25 19:11:44 -0300

    feat: first name on message campaign and campaign id on message

--- app/jobs/campaign_message_job.rb
+++ app/jobs/campaign_message_job.rb
@@ -1,30 +1,27 @@
 class CampaignMessageJob < ApplicationJob
   queue_as :high
 
-  def perform(account_id, inbox_id, content, audience)
+  # rubocop:disable Metrics/AbcSize
+  def perform(account_id, inbox_id, campaign_id, content, audience)
+    # rubocop:enable Metrics/AbcSize
     phone_number = audience[:phone_number]
     contact = Contact.find_by(phone_number: phone_number, account_id: account_id)
-
     contact = Contact.create!(phone_number: phone_number, account_id: account_id) if contact.blank?
-
     contact_inbox = ContactInboxBuilder.new(
-      contact: contact,
-      inbox: Inbox.find(inbox_id),
-      source_id: contact.phone_number.delete('+').to_s
+      contact: contact, inbox: Inbox.find(inbox_id), source_id: contact.phone_number.delete('+').to_s
     ).perform
-
     conversation = ConversationBuilder.new(params: {}, contact_inbox: contact_inbox).perform
-    audience[:first_name] = audience[:name] || '' if audience[:name]
-
+    audience[:first_name] = (audience[:name] || '').split[0]&.capitalize if audience[:name]
     m = content
     [:identifier, :first_name, :name, :due_at, :value, :scheduled_at].each do |f|
       k = f.to_sym
       m = m.gsub("##{f}", audience[k]) if audience[k]
     end
-
-    Messages::MessageBuilder.new(nil, conversation, {
-                                   content: m,
-                                   message_type: :outgoing
-                                 }).perform
+    conversation.messages.create!(
+      content: m, account_id: account_id,
+      content_type: :text, inbox_id: inbox_id,
+      message_type: :outgoing, status: :progress,
+      additional_attributes: { campaign_id: campaign_id }
+    )
   end
 end
