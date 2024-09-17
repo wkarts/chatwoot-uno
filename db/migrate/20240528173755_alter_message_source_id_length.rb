836b57efbddf56c2804dbeb5ce0e0bf22f217adc
Clairton Rodrigo Heinzen <clairton.rodrigo@gmail.com>
2024-09-17 16:49:13 -0300

    fix sintax on migration alter_message_source_id_length

--- db/migrate/20240528173755_alter_message_source_id_length.rb
+++ db/migrate/20240528173755_alter_message_source_id_length.rb
@@ -1,5 +1,5 @@
 class AlterMessageSourceIdLength < ActiveRecord::Migration[7.0]
   def change
-    change_column :messages, :source_id, :string, :limit => 510
+    change_column :messages, :source_id, :string, limit: 512
   end
 end
