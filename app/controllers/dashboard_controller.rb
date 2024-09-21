unoapi/chatwoot

feat: conversation style css

app/controllers/dashboard_controller.rb
@@ -31,7 +31,7 @@ class DashboardController < ActionController::Base
       'LOGOUT_REDIRECT_LINK',
       'DISABLE_USER_PROFILE_UPDATE',
       'DEPLOYMENT_ENV',
-      'CSML_EDITOR_HOST'
+      'CSML_EDITOR_HOST', 'CONVESATION_STYLE_CSS'
     ).merge(app_config)
   end
 

app/javascript/shared/store/globalConfig.js
@@ -20,6 +20,7 @@ const {
   WIDGET_BRAND_URL: widgetBrandURL,
   DISABLE_USER_PROFILE_UPDATE: disableUserProfileUpdate,
   DEPLOYMENT_ENV: deploymentEnv,
+  CONVESATION_STYLE_CSS: conversationStyleCss,
 } = window.globalConfig || {};
 
 const state = {
@@ -44,6 +45,7 @@ const state = {
   privacyURL,
   termsURL,
   widgetBrandURL,
+  conversationStyleCss: conversationStyleCss || '',
 };
 
 export const getters = {

wkarts/chatwoot-uno

app/controllers/dashboard_controller.rb
@@ -32,7 +32,7 @@ class DashboardController < ActionController::Base
       'LOGOUT_REDIRECT_LINK',
       'DISABLE_USER_PROFILE_UPDATE',
       'DEPLOYMENT_ENV',
-      'CSML_EDITOR_HOST'
+      'CSML_EDITOR_HOST', 'CONVERSATION_STYLE_CSS'
     ).merge(app_config)
   end
 

app/javascript/shared/store/globalConfig.js
@@ -20,6 +20,7 @@ const {
   WIDGET_BRAND_URL: widgetBrandURL,
   DISABLE_USER_PROFILE_UPDATE: disableUserProfileUpdate,
   DEPLOYMENT_ENV: deploymentEnv,
+  CONVERSATION_STYLE_CSS: conversationStyleCss,
 } = window.globalConfig || {};
 
 const state = {
@@ -44,6 +45,7 @@ const state = {
   privacyURL,
   termsURL,
   widgetBrandURL,
+  conversationStyleCss: conversationStyleCss || '',
 };
 
 export const getters = {